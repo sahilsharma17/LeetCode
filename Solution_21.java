b61a107ffe5ad0f693565bbba67234e6e735f9c5
sahilsharma17 <sahils17sam@gmail.com>
2023-03-27 22:52:42 +0530

    updated 21

--- Solution_21.java
+++ Solution_21.java
@@ -35,7 +35,7 @@ public class Solution_21 {
 class ListNode {
      int val;
      ListNode next;
-     ListNode() {}
+     ListNode() {};
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
