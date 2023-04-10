1f1e96c09f02ec153fc29f1f362de6679b0a4191
sahilsharma17 <sahils17sam@gmail.com>
2023-04-10 19:05:37 +0530

    Ques added

--- Solution_28.java
+++ Solution_28.java
@@ -1,5 +1,9 @@
-public class Solution_28 {
+/* Leetcode 28. Find the Index of the First Occurrence in a String
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+ * or -1 if needle is not part of haystack.
+ */
 
+public class Solution_28 {
     public static int strStr(String haystack, String needle){
     if (haystack.equals(needle)) return 0;
     char[] h = haystack.toCharArray();
