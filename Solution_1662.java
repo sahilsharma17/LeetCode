291ebe9f71d114efed8ff3310e6ba9930eae46d1
sahilsharma17 <sahils17sam@gmail.com>
2023-04-12 19:58:34 +0530

    ques added

--- Solution_1662.java
+++ Solution_1662.java
@@ -1,3 +1,8 @@
+/* Leetcode 1662. Check If Two String Arrays are Equivalent
+ * Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+ * A string is represented by an array if the array elements concatenated in order forms the string.
+ */
+
 import java.util.Arrays;
 
 public class Solution_1662{
