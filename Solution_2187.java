a8c4fae3558ede32a58c58a1a489bda832dd8d9d
sahilsharma17 <sahils17sam@gmail.com>
2023-04-13 20:21:06 +0530

    ques added

--- Solution_2187.java
+++ Solution_2187.java
@@ -1,3 +1,12 @@
+/* 2187. Minimum Time to Complete Trips
+ * You are given an array time where time[i] denotes the time taken by the ith bus to complete one trip.
+ * Each bus can make multiple trips successively; that is, the next trip can start immediately after
+ * completing the current trip. Also, each bus operates independently; that is,
+ * the trips of one bus do not influence the trips of any other bus.
+ * You are also given an integer totalTrips, which denotes the number of trips all buses should make in total.
+ * Return the minimum time required for all buses to complete at least totalTrips trips.
+ */
+
 public class Solution_2187 {
     public static long minimumTime(int[] time, int totalTrips) {
         long low = Long.MAX_VALUE, high = 0;
