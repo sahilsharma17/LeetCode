83ec19341aca92ca76862de3692bd7056a66562b
sahilsharma17 <sahils17sam@gmail.com>
2023-04-27 23:25:31 +0530

    Solved 319

--- Solution_319.java
+++ Solution_319.java
@@ -6,5 +6,10 @@
  */
 
 public class Solution_319 {
-    
+    public static int bulbSwitch(int n) {
+        return (int) Math.sqrt(n); 
+    }
+    public static void main(String[] args) {
+        System.out.println(bulbSwitch(3));
+    }
 }
