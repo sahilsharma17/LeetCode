1866c3efe641a78a8ebde54fdef8f68c5389eda9
sahilsharma17 <sahils17sam@gmail.com>
2023-05-03 23:02:34 +0530

    solved 169

--- Solution_169.java
+++ Solution_169.java
@@ -4,6 +4,32 @@
  * You may assume that the majority element always exists in the array.
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_169 {
-    
+    public static int majorityElement(int[] nums) {
+        int max = 0;
+        int majorityElem = 0;
+        Map<Integer, Integer> maps = new HashMap<>();
+
+        for (int num : nums) {
+            maps.put(num, maps.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : maps.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if(value > max){
+                max = value;
+                majorityElem = key;
+            }
+        }
+
+        return majorityElem;
+    }
+    public static void main(String[] args) {
+        int[] num = new int[]{2,2,1,1,1,2,2};
+        System.out.println(majorityElement(num));
+    }
 }
